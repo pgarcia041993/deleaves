ab50abffed9584c553222f4b088115d58a83794d
Mar Angel Nepomuceno <mar_22183@yahoo.com>
2014-05-19 11:11:14 +0800

    fixing the sorter

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,10 +15,10 @@ OnlineApplication::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
-  config.assets.digest = true
+  config.assets.digest = false
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
@@ -46,7 +46,7 @@ OnlineApplication::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( home.js global.js realtime.js online_application_forms.css table2CSV.js)
+  config.assets.precompile += %w( home.js global.js realtime.js online_application_forms.css table2CSV.js jquery.tablesorter.pager.css jquery.tablesorter.min.js jquery.tablesorter.pager.js jquery-latest.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
