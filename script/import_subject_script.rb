a5cf7598b418fd79ea450421ccda9c1333be8f3a
Mar Angel Nepomuceno <mar_22183@yahoo.com>
2014-05-21 13:43:39 +0800

    almost finish

--- script/import_subject_script.rb
+++ script/import_subject_script.rb
@@ -2,47 +2,37 @@ require 'csv'
 headers = {}
 students = {}
 grades = {}
-
 counter = 0
+Subject.delete_all
 CSV.foreach("#{Rails.root}/#{ARGV[0]}") do |row|
   counter = counter + 1
-  col_counter=0
-  row.each do |col|
 
-    headers.merge!(col_counter => col)
-    col_counter= col_counter + 1
+  if counter == 0
+    col_counter = 0
+    row.each do |col|
+      #puts col
+      if col_counter == 0
+      else
+        headers.merge!(col_counter => col) unless col == 'GPA'
+      end
+      col_counter = col_counter + 1
+    end
+  else
 
-
+    headers.each_pair do |k, v|
+      grades.merge!(v => row[k])
+    end
+    students.merge!(row[0] =>row[1])
   end
-
-   headers.each_pair do |k, v|
-    grades.merge!(v => row[k])
-  end
-  #if counter < 2
-  #  next
-  #elsif counter == 2
-  #  col_counter = 0
-  #  row.each do |col|
-  #    puts col
-  #    if col_counter == 0
-  #    else
-  #      headers.merge!(col_counter => col) unless col == 'GPA'
-  #    end
-  #    col_counter = col_counter + 1
-  #  end
-  #else
-  #  grades = {}
-  #  headers.each_pair do |k, v|
-  #    grades.merge!(v => row[k])
-  #  end
-  #  students.merge!(row[0] => grades)
-  #end
 
 end
-puts headers
-#puts grades
-#puts students
-#
+
+#puts headers
+
+
+students.each_pair do |student,code|
+  Subject.create!(subject_code: code, description:student)
+end
 #students.each_pair do |student, scores|
 #  ##student_search = Student.where(number: student.keys.first).first
 #  #if student_search.blank?
@@ -51,7 +41,7 @@ puts headers
 #
 #  #puts student_search
 #  scores.each_pair do |subject, score|
-#    #Grade.create!(student_id: student_search.id, temporary_subject_code: subject, grade: score)
+#Grade.create!(student_id: student_search.id, temporary_subject_code: subject, grade: score)
 #    puts "#{student.keys.first} : #{student[student.keys.first]} : #{subject} : #{score}"
 #  end
 #end
