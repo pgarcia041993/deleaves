2eecf4b46afcc12acd402d71110596a6948e2f48
Mar Angel Nepomuceno <mar_22183@yahoo.com>
2014-05-23 15:23:43 +0800

    almost finish v2

--- script/import_csv_script.rb
+++ script/import_csv_script.rb
@@ -3,8 +3,8 @@ headers = {}
 students = {}
 names= {}
 units = []
-Student.delete_all
-Grade.delete_all
+#Student.delete_all
+#Grade.delete_all
 
 counter = 0
 CSV.foreach("#{Rails.root}/#{ARGV[0]}") do |row|
@@ -54,10 +54,9 @@ end
 students.each_pair do |student, scores|
   puts(student)
 
-  student_search = Student.where(number: student.keys.first).first
-  if student_search.blank?
+
     student_search = Student.create!(number: student.keys.first, name: student[student.keys.first][:name], cohort: student[student.keys.first][:cohort], term: student[student.keys.first][:term], learning_team: student[student.keys.first][:lt], gpa: student[student.keys.first][:gpa], rank: student[student.keys.first][:rank], fail: student[student.keys.first][:fail], scholar: student[student.keys.first][:scholar])
-  end
+
   scores.each_pair do |subject, score|
     Grade.create!(student_id: student_search.id, temporary_subject_code: subject, grade: score[1], units: score[0])
     puts "#{student.keys.first} : #{student[student.keys.first]} : #{subject} : #{score}"
