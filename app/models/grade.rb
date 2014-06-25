80864b567b168ebad35fc135a23add96eca5d115
Mar Angel Nepomuceno <mar_22183@yahoo.com>
2014-06-25 15:38:36 +0800

    added column whatif grades

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,4 +1,4 @@
 class Grade < ActiveRecord::Base
-  attr_accessible :grade, :student_id, :subject_id, :temporary_subject_code , :units
+  attr_accessible :grade, :student_id, :subject_id, :temporary_subject_code , :units , :whatif
   belongs_to :student
 end
